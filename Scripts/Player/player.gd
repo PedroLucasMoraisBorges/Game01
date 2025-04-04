79c7bafa49ecb44865c1e12f08d9b57275af241f
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2025-04-04 19:05:42 -0300

    att_anim

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,12 +2,8 @@ extends CharacterBody2D
 class_name Player
 signal healthChanged
 
-@export var speed: int = 300
 var is_attacking: bool = false
-var gravity = 980
 const jump_velocity = -400.0
-@onready var sprites: AnimatedSprite2D = $Sprites
-@onready var hurtTimer = $HurtTimer
 @export var knockBackPower: int = 500
 @onready var hurtbox = $HurtBox/CollisionShape2D
 @onready var hurtbox_initial_position = hurtbox.position
@@ -15,7 +11,6 @@ const jump_velocity = -400.0
 @onready var hitbox_initial_position = hitbox.position
 @onready var collision = $Collision
 @onready var collision_initial_position = collision.position
-
 var isHurt: bool = false
 
 var foot_step_frames = [1,5]
@@ -27,73 +22,105 @@ var foot_step_frames = [1,5]
 
 @onready var sfx_player = %sfx_player
 
-func _ready() -> void:
-	hitbox.disabled = true
-	
-func load_sfx(sfx_to_load):
-	if sfx_player.stream != sfx_to_load:
-		sfx_player.stop()
-		sfx_player.stream = sfx_to_load
+@export var animation_tree : AnimationTree
 
-func HandleInput(delta):
-	velocity.x = 0
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprites.play("fall")
-		
-	# Entrada para pular
-	if is_on_floor() and Input.is_action_just_pressed("ui_up") and not is_attacking:
-		velocity.y = jump_velocity
-		sprites.play("jump")
-		await  sprites.animation_finished
+var state_machine: AnimationNodeStateMachinePlayback
+var move_state_machine : AnimationNodeStateMachinePlayback
+var jump_state_machine : AnimationNodeStateMachinePlayback
+var attack_state_machine : AnimationNodeStateMachinePlayback
 
-	# Entrada para correr
-	var moving = false
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-		sprites.flip_h = true
-		moving = true
-		adjust_hitbox_position()
-		
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		sprites.flip_h = false
-		moving = true
-		adjust_hitbox_position()
-	
-	if Input.is_action_just_pressed("attack") and is_on_floor() and not is_attacking:
-		attack()
-	# Definir animação se estiver no chão
-	if is_on_floor() and not is_attacking and not isHurt:  # Apenas muda animações se não estiver atacando
-		if moving:
-			sprites.play("run")
+var gravity = 980
+
+var speed: float = 175
+var direction : float
+var counter : int = 0
+var delay : float
+var on_floor : bool:
+	set(value):
+		if value == on_floor:
+			if value == true:
+				flip_sprite()
+			return
+		on_floor = value
+		if value == true:
+			state_machine.travel("Movement")
 		else:
-			sprites.play("idle")
+			state_machine.travel("Jump")
 
-func _physics_process(delta: float) -> void:
-	HandleInput(delta)
-	move_and_slide()
+func _ready() -> void:
+	state_machine = animation_tree.get("parameters/playback")
+	move_state_machine = animation_tree.get("parameters/Movement/playback")
+	jump_state_machine = animation_tree.get("parameters/Jump/playback")
+	attack_state_machine = animation_tree.get("parameters/Attack/playback")
 
-func attack():
-	is_attacking = true
-	velocity.x = 0
-	sprites.play("attack")
-	hitbox.disabled = false
-	await sprites.animation_finished
-	hitbox.disabled = true
-	is_attacking = false
-	load_sfx(sfx_attack)
-	sfx_player.play()
+func set_motion(value : bool):
+	animation_tree.set("parameters/Movement/conditions/is_moving", value)
+	animation_tree.set("parameters/Movement/conditions/is_stopped", not value)
 	
-func adjust_hitbox_position():
-	if sprites.flip_h:
-		hitbox.position.x = -abs(hitbox_initial_position.x) + 24
-		hurtbox.position.x = -abs(hurtbox_initial_position.x) + 24
-		collision.position.x = -abs(collision_initial_position.x) + 24
+func set_speed(value: float):
+	speed = value
+	
+func flip_sprite():
+	if direction < 0:
+		$Sprite2D.flip_h = true
+	elif direction > 0:
+		$Sprite2D.flip_h = false
+		
+func play_attack(type: String):
+	attack_state_machine.travel("Attack_"+ type)
+	state_machine.travel("Attack")
+	set_speed(0)
+	
+func controls():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		state_machine.travel("Jump")
+		velocity.y = -450
+	
+	if Input.is_action_just_pressed("roll") and is_on_floor():
+		move_state_machine.travel("Roll")
+		set_speed(50.0)
+	
+	if Input.is_action_just_pressed("attack_right"):
+		play_attack("03")
+	
+	if Input.is_action_just_pressed("attack_left") and delay <= 0:
+		delay = 0.75
+		$RESET.start()
+		
+		if is_on_floor():
+			counter += 1
+			attack((counter % 3 == 0))
+		if not is_on_floor():
+			jump_state_machine.travel("JumpAtdawatack")
+	
+	if Input.is_action_just_pressed("ultimate") and is_on_floor():
+		# state_machine.travel("GroupAttack/Attack01")
+		play_attack("special")
+
+func attack(is_third):
+	if is_third:
+		play_attack("02")
+		counter = 0
+		return
+	play_attack("01")
+	
+func _physics_process(delta: float) -> void:
+	if delta > 0:
+		delay -= delta
+		
+	direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+	velocity.y += gravity * delta
+	move_and_slide()
+	
+	on_floor = is_on_floor()
+	
+	if velocity == Vector2.ZERO:
+		set_motion(false)
 	else:
-		hitbox.position.x = abs(hitbox_initial_position.x) 
-		hurtbox.position.x = abs(hurtbox_initial_position.x)
-		collision.position.x = abs(collision_initial_position.x)
+		set_motion(true)
+		
+	controls()
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	isHurt = true
@@ -103,17 +130,8 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 		Global.currentHealth = Global.maxHealth
 		
 	Global.healthChanged.emit()
-	
-	sprites.play("hurt")
-	await sprites.animation_finished
-	isHurt = false
-	
 
-func _on_sprites_frame_changed() -> void:
-	if sprites.animation == "idle": return
-	if sprites.animation == "jump": return
-	if sprites.animation == "fall": return
-	if sprites.animation == "attack": return
-	load_sfx(sfx_run)
-	
-	if sprites.frame in foot_step_frames: sfx_player.play()
+	isHurt = false
+
+func _on_reset_timeout() -> void:
+	counter = 0
